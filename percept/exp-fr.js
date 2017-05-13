ae17f1fb0f5e6d35700c4aa0e0a0609c97e250d0
Hugues Lacroix <hlacroix>
2017-05-13 13:54:57 -0400

    fixed other typos in french form

--- percept/exp-fr.js
+++ percept/exp-fr.js
@@ -163,13 +163,13 @@ function runExperiment(){
 				"sex" :  {type: "radio", labels: ["Masculin", "Féminin"], question:"Sexe", required: true, errorInfo:"* Réponse obligatoire"},
 				"birth_date" :  {type: "date", label:' ', question: "Date de naissance", required: true, errorInfo:"* Réponse obligatoire"},
 				"birth_place" :  {type: "text", label:" ", question: "Lieu de naissance", required: true, errorInfo:"* Réponse obligatoire"},
-				"school_level" :  {type: "text", label: "Secondaire, cégep, bacc., maitrise, doctorat, etc.", question: "Niveau de scolarité", required: true, errorInfo:"* Réponse obligatoire"},
+				"school_level" :  {type: "text", label: "Secondaire, cégep, bacc., maitrise, etc.", question: "Niveau de scolarité", required: true, errorInfo:"* Réponse obligatoire"},
 				"school_language" :  {type: "text", label: " ", question:"Langue principale d'enseignment", required: true, errorInfo:"* Réponse obligatoire"},
 				"origin_mother" :  {type: "text", label: " ", question:"Pays d'origine et langue maternelle de votre mère", required: true, errorInfo:"* Réponse obligatoire"},
 				"origin_father" :  {type: "text", label: " ", question:"Pays d'origine et langue maternelle de votre père", required: true, errorInfo:"* Réponse obligatoire"},
 				"mother_tongue" :  {type: "text", label: " ", question:"Quelle est votre langue maternelle", required: true, errorInfo:"* Réponse obligatoire"},
 				"other_languages" :  {type: "text", label: " ", question:"Quelles autres langues parlez vous et à quel niveau?", required: true, errorInfo:"* Réponse obligatoire"},
-				"other_languages" :  {type: "textarea", placeholder: "L2: Anglais, niveau avancé. L3: Japonais, niveau débutant", question: "Quelle autres langues parlez vous et à quel niveau?", required: true, errorInfo:"* Réponse obligatoire (vous pouvez écrire <em>aucune</em>)", cols:60},
+				"other_languages" :  {type: "textarea", placeholder: "Par exemple : <br />\n L2: Anglais, niveau avancé. <br />\nL3: Japonais, niveau débutant", question: "Quelle autres langues parlez vous et à quel niveau?", required: true, errorInfo:"* Réponse obligatoire (vous pouvez écrire <em>aucune</em>)", cols:60},
 				onSubmit: {label: "Continuer", onclick: function(){}}
 			}
 		});
